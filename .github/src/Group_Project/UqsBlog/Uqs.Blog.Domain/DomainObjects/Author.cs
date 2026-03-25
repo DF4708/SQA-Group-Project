273d12f3d272965bb6e6967261f2c861da713d46
DF4708 <138391381+DF4708@users.noreply.github.com>
2026-03-24 21:57:08 -0500

    Delete .github/src/Group_Project/UqsBlog directory

--- .github/src/Group_Project/UqsBlog/Uqs.Blog.Domain/DomainObjects/Author.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Uqs.Blog.Domain.DomainObjects;
-
-public class Author
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public bool IsLocked { get; set; }
-    public Post[]? Posts { get;set; }
-}
--- .github/src/Group_Project/UqsBlog/Uqs.Blog.Domain/DomainObjects/Tag.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Uqs.Blog.Domain.DomainObjects;
-
-public struct Tag
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
