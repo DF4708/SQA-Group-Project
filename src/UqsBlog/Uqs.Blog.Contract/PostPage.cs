d836bf03514de90a56913d6f498b58511e0f6e87
DF4708 <138391381+DF4708@users.noreply.github.com>
2026-03-24 21:00:33 -0500

    Delete src directory

--- src/UqsBlog/Uqs.Blog.Contract/PostPage.cs
+++
@@ -1,4 +0,0 @@
-﻿namespace Uqs.Blog.Contract;
-
-public record PostPage(int Id, string Content, Author Author, DateTime CreatedDate, int NumberOfComments, int NumberOfViews);
-
--- src/UqsBlog/Uqs.Blog.Domain/DomainObjects/Comment.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Uqs.Blog.Domain.DomainObjects;
-
-public class Comment
-{
-    public int Id { get; set; }
-    public string? Text { get; set; }
-    public DateTimeOffset CreatedDate { get; set; }
-}
